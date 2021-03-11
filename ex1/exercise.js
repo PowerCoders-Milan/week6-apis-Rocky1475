42a6ae447dfa4daecb70d97286798bbc42e4e7d1
Muhammad rRehman <muhammadislamian@gmail.com>
2021-03-11 14:04:30 +0000

    EX 1 done

---
+++ ex1/exercise.js
@@ -0,0 +1,9 @@
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 45.464203, lng: 9.189982 },
+        zoom: 8
+        
+    });
+    
+}
